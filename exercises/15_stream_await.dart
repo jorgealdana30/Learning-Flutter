a64278f47379f0745df1ad0b34c61a411b509308
Jorge Aldana <jorge.aldana@JORGEALDANA.local>
2023-06-18 17:29:33 -0500

    [feature]: añadiendo nuevos ejercicios

---
+++ exercises/15_stream_await.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  emitNumbers().listen((value) {
+    print('Stream Value: $value');
+  });
+}
+
+Stream<int> emitNumbers() async* {
+  
+  final valuesToEmit = [1,2,3,4,5];
+  
+  for(int i in valuesToEmit){
+    await Future.delayed(const Duration(seconds: 1));
+    yield i;
+  }
+}
